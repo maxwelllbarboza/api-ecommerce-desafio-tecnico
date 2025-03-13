7ed65bb4faeaebc83f5d3492e560c54b5c123d50
Maxwell <Maxwellbarboza@hotmail.com>
2025-03-13 19:50:00 -0300

    Alterando jwt

--- src/modules/configs/security/jwt/jwt.token.module.ts
+++ src/modules/configs/security/jwt/jwt.token.module.ts
@@ -5,11 +5,11 @@ import { JwtTokenService } from './jwt.token.service';
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
+      secret: process.env.JWT_SECRET || '74YLbq4%c!wU',
       signOptions: { expiresIn: process.env.JWT_EXPIRATION_TIME || '2h' },
     }),
     JwtModule.register({
-      secret: process.env.JWT_REFRESH_TOKEN_SECRET,
+      secret: process.env.JWT_REFRESH_TOKEN_SECRET || '7jML9q4-c!s0',
       signOptions: {
         expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME || '24h',
       },
